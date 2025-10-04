9aff56bdf19877068d3031e5e33ba94e75f0ba71
tz1211 <terryzhou1211@gmail.com>
2025-10-03 21:18:20 -0400

    create initial frontend project

---
+++ frontend/start-frontend-container.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# exit immediately if a command exits with a non-zero status
+set -e
+
+export BASE_DIR=$(pwd)
+export IMAGE_NAME="datadetox-frontend"
+
+# Build the image based on the Dockerfile
+echo "Building docker image..."
+docker build -t $IMAGE_NAME -f Dockerfile .
+
+# Run Container
+echo "Run Docker container..."
+docker run --rm --name $IMAGE_NAME -ti \
+-v $BASE_DIR:/app \
+$IMAGE_NAME
